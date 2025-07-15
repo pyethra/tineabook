3bf906b3cc5d2703f677a04e9ef43d7628930536
Pyethra (PC Casa) <pyethradelcolribeiro@gmail.com>
2025-07-14 23:17:15 -0300

    Tudo certo

--- tineabook-app/App.js
+++ tineabook-app/App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {Pesquisa} from './src/features/PesquisarLivros/index'
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <Pesquisa>
+      </Pesquisa>
     </View>
   );
 }
